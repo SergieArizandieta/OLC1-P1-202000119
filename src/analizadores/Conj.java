c936b706258d5d77136fdea8e486456042a68aab
Serch-ING <3000523830101@ingenieria.usac.edu.gt>
2022-03-06 01:01:17 -0600

    agregar char especiales

--- src/analizadores/Conj.java
+++ src/analizadores/Conj.java
@@ -92,14 +92,14 @@ public class Conj {
 				validado = true;
 				break;
 			}else {
-				if(i.length()>2) {
-				String temptext = i;
-				temptext = i.substring(0,0) + i.substring(0+1);
-				temptext = temptext.substring(0,temptext.length()-1);
-				if(temptext.equals(letter)) {
-					validado = true;
-					break;
-				}
+				if (i.length() > 2) {
+					String temptext = i;
+					temptext = i.substring(0, 0) + i.substring(0 + 1);
+					temptext = temptext.substring(0, temptext.length() - 1);
+					if (temptext.equals(letter)) {
+						validado = true;
+						break;
+					}
 				}
 			}
 		}
